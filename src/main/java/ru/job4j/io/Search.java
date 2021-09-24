8f67f8320b2624fef6669c2b1015b08be4d1590f
RaduKostashchuk <kostasc@mail.ru>
2021-09-24 19:36:18 +0700

    5. Валидация параметров запуска. [#246865 #207754]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,14 +9,7 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        if (args.length != 2) {
-            throw new IllegalArgumentException("Wrong argument number. "
-                    + "Usage java -jar search.jar START_FOLDER FILE_EXTENSION");
-        }
-        if (!args[1].matches("^[0-9a-zA-Z]+$")) {
-            throw new IllegalArgumentException("Illegal file extension format. "
-                    + "Should be one or more characters of [0-9a-zA-Z] pattern.");
-        }
+        validate(args);
         Path start = Paths.get(args[0]);
         search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
@@ -25,5 +18,16 @@ public class Search {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static void validate(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Wrong argument number. "
+                    + "Usage java -jar search.jar START_FOLDER FILE_EXTENSION");
+        }
+        if (!args[1].matches("^[0-9a-zA-Z]+$")) {
+            throw new IllegalArgumentException("Illegal file extension format. "
+                    + "Should be one or more characters of [0-9a-zA-Z] pattern.");
+        }
     }
 }
