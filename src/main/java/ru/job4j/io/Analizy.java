682f813403757a50da306188fd5b17469008d78e
RaduKostashchuk <kostasc@mail.ru>
2021-09-24 12:37:58 +0700

    2. Анализ доступности сервера. [#859 #207186]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -1,38 +1,29 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class Analizy {
     public void unavailable(String source, String target) {
-        List<String> lines = new ArrayList<>();
-        List<String> result = new ArrayList<>();
-        String[] line;
+        String line, last = "";
         boolean isIn = false;
-        try (BufferedReader in = new BufferedReader(new FileReader(source))) {
-            lines = in.lines().collect(Collectors.toList());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        for (int i = 0; i < lines.size(); i++) {
-            line = lines.get(i).split(" ");
-            if (line[0].matches("400|500")) {
-                if (!isIn || i == lines.size() - 1) {
-                    result.add(line[1]);
+        try (BufferedReader in = new BufferedReader(new FileReader(source));
+             PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
+            while ((line = in.readLine()) != null) {
+                if (line.matches("^400.+|^500.+")) {
+                    if (!isIn) {
+                        out.print(line.split(" ")[1] + ";");
+                    }
+                    isIn = true;
+                    last = line;
+                    continue;
                 }
-                isIn = true;
-                continue;
+                if (!line.matches("^400.+|^500.+") && isIn) {
+                    out.println(line.split(" ")[1] + ";");
+                    isIn = false;
+                }
             }
-            if (!line[0].matches("400|500") && isIn) {
-                result.add(line[1]);
-                isIn = false;
-            }
-        }
-        try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
-            for (int i = 0; i < result.size(); i += 2) {
-                out.println(result.get(i) + ";" + result.get(i + 1) + ";");
+            if (isIn && last.matches("^400.+|^500.+")) {
+                out.println(last.split(" ")[1]);
             }
         } catch (Exception e) {
             e.printStackTrace();
