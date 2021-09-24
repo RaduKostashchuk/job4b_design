db4bda277988d4e30a5651a45f3590ec0087fe2a
RaduKostashchuk <kostasc@mail.ru>
2021-09-24 15:17:48 +0700

    2. Анализ доступности сервера. [#859 #207186]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -4,27 +4,38 @@ import java.io.*;
 
 public class Analizy {
     public void unavailable(String source, String target) {
+        StringBuilder result = new StringBuilder();
         String line, last = "";
         boolean isIn = false;
-        try (BufferedReader in = new BufferedReader(new FileReader(source));
-             PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
+        try (BufferedReader in = new BufferedReader(new FileReader(source))) {
             while ((line = in.readLine()) != null) {
                 if (line.matches("^400.+|^500.+")) {
                     if (!isIn) {
-                        out.print(line.split(" ")[1] + ";");
+                        result.append(line.split(" ")[1])
+                                .append(";");
                     }
                     isIn = true;
                     last = line;
                     continue;
                 }
                 if (!line.matches("^400.+|^500.+") && isIn) {
-                    out.println(line.split(" ")[1] + ";");
+                    result.append(line.split(" ")[1])
+                            .append(";")
+                            .append(System.lineSeparator());
                     isIn = false;
                 }
             }
             if (isIn && last.matches("^400.+|^500.+")) {
-                out.println(last.split(" ")[1]);
+                result.append(last.split(" ")[1])
+                        .append(";")
+                        .append(System.lineSeparator());
             }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
+            out.println(result);
         } catch (Exception e) {
             e.printStackTrace();
         }
