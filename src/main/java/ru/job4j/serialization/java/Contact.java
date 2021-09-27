b1af4d3b06b81d3b97943e0b1e9902987b7f5621
RaduKostashchuk <kostasc@mail.ru>
2021-09-27 18:43:41 +0700

    1. Что такое Сериализация? [#313163 #208931]

--- src/main/java/ru/job4j/serialization/java/Contact.java
+++ src/main/java/ru/job4j/serialization/java/Contact.java
@@ -33,14 +33,14 @@ public class Contact implements Serializable {
         final Contact contact = new Contact(330110, "8-913-389-30-76");
         File tempFile = Files.createTempFile(null, null).toFile();
         try (FileOutputStream fos = new FileOutputStream(tempFile);
-            ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
                 oos.writeObject(contact);
             } catch (Exception e) {
             e.printStackTrace();
         }
 
-        try (FileInputStream fis = new FileInputStream(tempFile)) {
-            ObjectInputStream ois = new ObjectInputStream(fis);
+        try (FileInputStream fis = new FileInputStream(tempFile);
+            ObjectInputStream ois = new ObjectInputStream(fis)) {
             final Contact contactFromFile = (Contact) ois.readObject();
             System.out.println(contactFromFile);
         } catch (Exception e) {
